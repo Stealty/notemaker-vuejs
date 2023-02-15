da2d073e46475110fc173685b47d8b3092ad556e
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-15 13:46:19 -0300

    feat: ViewStats page

--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useStoreNotes } from "@/stores/globalStore";
+
+const storeNotes = useStoreNotes();
+</script>
 
 <template>
-  <main>
-    <h1>Stats</h1>
-  </main>
+  <div class="stats">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Stat</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Number of notes</td>
+          <td>{{ storeNotes.totalNotesCount }}</td>
+        </tr>
+        <tr>
+          <td>Number of Character (all notes)</td>
+        </tr>
+        <td>{{ storeNotes.totalCharactersCount }}</td>
+      </tbody>
+    </table>
+  </div>
 </template>
