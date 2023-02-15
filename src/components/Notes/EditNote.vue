cb2da1fa4316a91a7116ee914a936c2013df3146
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-15 13:45:33 -0300

    feat: EditNote component

---
+++ src/components/Notes/EditNote.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import router from "@/router";
+import { useStoreNotes } from "@/stores/globalStore";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const storeNotes = useStoreNotes();
+const noteInputRef = ref<any>();
+const noteInputValue = ref(storeNotes.getNoteContent(route.params.id as any));
+
+function editNote() {
+  storeNotes.editNote(route.params.id as any, noteInputValue.value as any);
+  noteInputRef.value.focus();
+  router.back();
+}
+</script>
+
+<template>
+  <div class="card p-4 mb-5 has-background-link-dark">
+    <label class="label has-text-white">Edit note</label>
+    <div class="field">
+      <div class="control">
+        <textarea
+          ref="noteInputRef"
+          v-model="noteInputValue"
+          class="textarea"
+          placeholder="Add note update"
+        />
+      </div>
+    </div>
+
+    <div class="field is-grouped is-grouped-right">
+      <div class="control">
+        <button @click="$router.back()" class="button is-link is-light mr-4">
+          Cancel
+        </button>
+        <button
+          :disabled="!noteInputValue.trim()"
+          @click="editNote"
+          class="button is-link has-background-link"
+        >
+          Save note
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
