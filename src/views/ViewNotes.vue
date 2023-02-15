336dd87866bc4de1096d2ba23b0da5ff711e6db2
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2023-02-15 13:46:02 -0300

    feat: ViewNotes page

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,76 +1,17 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import Note from "@/components/Notes/Note.vue";
+import { useStoreNotes } from "@/stores/globalStore";
 
-const notes = ref([
-  {
-    id: "",
-    content: "",
-  },
-]);
+import AddNote from "@/components/Notes/AddNote.vue";
 
-const noteInputValue = ref("");
-
-const noteInputRef = ref<any>();
-
-function randomId() {
-  let currentDate = new Date().getTime();
-  let id = currentDate.toString();
-  return id;
-}
-
-function addNewNote(value: any) {
-  const note = {
-    id: randomId(),
-    content: value,
-  };
-  notes.value.unshift(note);
-
-  noteInputValue.value = "";
-  noteInputRef.value.focus();
-}
-
-function deleteNote(id: any) {
-  notes.value = notes.value.filter((note) => {
-    return note.id !== id;
-  });
-}
+const storeNotes = useStoreNotes();
 </script>
 
 <template>
   <main>
     <div class="notes">
-      <div class="card has-background-success-dark p-4 mb-5">
-        <div class="field">
-          <div class="control">
-            <textarea
-              ref="noteInputRef"
-              v-model="noteInputValue"
-              class="textarea"
-              placeholder="Add a new note"
-            />
-          </div>
-        </div>
-
-        <div class="field is-grouped is-grouped-right">
-          <div class="control">
-            <button
-              :disabled="!noteInputValue.trim()"
-              @click="addNewNote(noteInputValue)"
-              class="button is-link has-background-success"
-            >
-              Add New Note
-            </button>
-          </div>
-        </div>
-
-        <Note
-          v-for="note in notes"
-          key="note.id"
-          :note="note"
-          @delete-note="deleteNote"
-        />
-      </div>
+      <AddNote />
+      <Note v-for="note in storeNotes.notes" key="note.id" :note="note" />
     </div>
   </main>
 </template>
